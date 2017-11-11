68bdeeabc5219f231ece40955f03b4f5d25662b2
thatchayut <thatchayut.u@gmail.com>
2017-11-11 17:02:01 +0700

    craete state struct and print state function

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -3,8 +3,37 @@
 #include <sstream>
 #include <vector>
 
+#define SIZE_OF_MEMORY 65536
+#define SIZE_OF_REGISTER 8 
+
 using namespace std;
 
+struct State {
+    int program_counter;
+    int memories[SIZE_OF_MEMORY];
+    int registers[SIZE_OF_REGISTER];
+    int number_of_instructions;
+} state;
+
+void printState(State &state) {
+    cout << "\n@@@\nstate:\n";
+    cout << "\tpc "<<state.program_counter<<endl;
+    cout << "\tmemory:\n";
+
+    for (int i = 0; i < state.number_of_instructions; i++)
+    {
+        cout << "\t\tmem[ " << i << " ] " << state.memories[i] << endl;
+    }
+
+    cout << "\tregisters:\n";
+    
+    for (int i = 0; i < SIZE_OF_REGISTER; i++)
+    {
+        cout << "\t\treg[ " << i << " ] " << state.registers[i] << endl;
+    }
+
+    cout << "end state" << endl;
+}
 vector<string> file_to_lines(ifstream &file) {
     vector<string> lines;
     string line;
